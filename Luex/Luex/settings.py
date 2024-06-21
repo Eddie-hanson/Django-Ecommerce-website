f63c2ddf7c1d785d4a0da18819b2fb3d84f9a2e3
Edward Hanson <hans18edward@gmail.com>
2024-06-21 12:18:13 +0000

    Configured a postgres da

--- Luex/Luex/settings.py
+++ Luex/Luex/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Luex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'LUEX',
+        'USER': 'postgres',
+        'PASSWORD': 'Edwardhans18.',
+        'HOST': 'Localhost',
     }
 }
 
