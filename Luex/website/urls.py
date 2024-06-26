15058c7495f1d1dff87882afa827f50b6d8e0c73
Edward Hanson <hans18edward@gmail.com>
2024-06-26 05:48:42 +0000

    Styled the product page

--- Luex/website/urls.py
+++ Luex/website/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='Home'),
-
+    path('Products/', views.Products, name='Products'),
     path('About/', views.About, name='about'),
     path('ProductDetails/', views.ProductDetails, name='ProductDetails'),
     path('cart/', views.Cart, name='cart'),
-
 ]
